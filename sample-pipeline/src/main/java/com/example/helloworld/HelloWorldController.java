3875d0724c1ef6c2f12961745c45fcf228fc30b0
Joshua Mathianas <jmathian@jmathian.remote.csb>
2019-02-28 14:55:26 -0500

    adding example of how different environment configmaps can be used

--- sample-pipeline/src/main/java/com/example/helloworld/HelloWorldController.java
+++ sample-pipeline/src/main/java/com/example/helloworld/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.example.helloworld;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class HelloWorldController {
+	@Value("${environment}")
+	String env;
+	
 	@RequestMapping(method=RequestMethod.GET)
 	public String sayHello() {
-		return "Hello from Spring";
+		return "Hello from Spring in environment " + env;
 	}
 }
